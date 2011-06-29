c53763f6ab7da0d269ffa3861d2c56eec2941f5c
Rotem Yaari <vmalloc@gmail.com>
2011-06-29 11:52:40 +0300

    Ignore pyc files that changed without their 'py' files having changed

--- redgreen/directory_change_iterator.py
+++ redgreen/directory_change_iterator.py
@@ -23,6 +23,7 @@ class DirectoryChangeIterator(object):
             changed = set(filename
                           for filename in set(new_state) | set(self._state)
                           if self._state.get(filename) != new_state.get(filename))
+            self._discard_pyc_of_unchanged_files(new_state, changed)
             if changed:
                 yield changed
                 self._state = new_state
@@ -49,4 +50,16 @@ class DirectoryChangeIterator(object):
             if filename.endswith(ext):
                 return True
         return False
-
+    def _discard_pyc_of_unchanged_files(self, new_state, change):
+        to_discard = set()
+        for filename in change:
+            if filename.endswith(".pyc"):
+                orig_filename = filename[:-1]
+                # properly handle cases where the pyc got deleted - this is important for missing modules that still
+                # have their .pyc in place
+                if orig_filename not in change:
+                    # the original filename hasn't changed...
+                    if filename in new_state:
+                        to_discard.add(filename)
+        for x in to_discard:
+            change.discard(x)
