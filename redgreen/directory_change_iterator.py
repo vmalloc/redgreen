a0b86a3faf85807ec579500be9d2958f13bd839f
Rotem Yaari <vmalloc@gmail.com>
2011-06-29 09:09:54 +0300

    Accept .pyc files as well as .py (useful when cleaning up .pyc's of removed paths or modules)

--- redgreen/directory_change_iterator.py
+++ redgreen/directory_change_iterator.py
@@ -1,9 +1,13 @@
+import functools
 import time
 import os
 
 def _get_file_key(filename):
     stat = os.stat(filename)
     return (stat.st_mtime, stat.st_size)
+
+def _make_suffix_predicate(suffix):
+    return functools.partial(str.endswith, suffix=suffix)
 
 class DirectoryChangeIterator(object):
     def __init__(self, root, sleep, exclude_dirs=()):
@@ -12,6 +16,7 @@ class DirectoryChangeIterator(object):
         self._root = root
         self._sleep = sleep
         self._exclude_dirs = set(os.path.relpath(d, self._root) for d in exclude_dirs)
+        self._accepted_extensions = [".py", ".pyc"]
     def __iter__(self):
         while True:
             new_state = self._get_state()
@@ -24,16 +29,24 @@ class DirectoryChangeIterator(object):
             time.sleep(self._sleep)
     def _get_state(self):
         returned = {}
+        for filename in self._walk_filenames():
+            try:
+                returned[filename] = _get_file_key(filename)
+            except (OSError, IOError):
+                continue
+        return returned
+    def _walk_filenames(self):
         for dirname, dirnames, filenames in os.walk(self._root):
             dirname = os.path.relpath(dirname, self._root)
             if dirname in self._exclude_dirs:
                 continue
             for filename in filenames:
-                if not filename.endswith(".py"):
+                if not self._is_extension_accepted(filename):
                     continue
-                full_filename = os.path.abspath(os.path.join(self._root, dirname, filename))
-                try:
-                    returned[full_filename] = _get_file_key(full_filename)
-                except (OSError, IOError):
-                    continue
-        return returned
+                yield os.path.abspath(os.path.join(self._root, dirname, filename))
+    def _is_extension_accepted(self, filename):
+        for ext in self._accepted_extensions:
+            if filename.endswith(ext):
+                return True
+        return False
+
