6d12dd0ad1bdf329ae4991e31eb986fc644193bb
Guy Rozendorn <guy@rzn.co.il>
2011-08-07 02:08:51 +0300

    adding support for multiple targets
    this allows monitoring projects where:
    * tests and code does not sit together and there's too many directories to exclude in their shared parent directory
    * docss and code ...
    
    .
    (cherry picked from commit 91e7fdc139671014de059b404ed8d8dae2dd3b23)
    
    Conflicts:
    
    	redgreen/scripts/__init__.py

--- redgreen/scripts/__init__.py
+++ redgreen/scripts/__init__.py
@@ -12,7 +12,7 @@ def get_parser():
     parser.add_argument("-v", "--verbose", action="store_true", default=False)
     parser.add_argument("-e", "--exclude-dir", dest="exclude_dirs", action="append", default=[])
     parser.add_argument("-d", "--run-in-dir", dest="run_in_dir", default=None, help="Specifies a directory in which to run nose (defaults to the monitored directory)")
-    parser.add_argument("-m", "--monitor-target", dest="monitored_target", default=".", help="Directory or file to monitor")
+    parser.add_argument("-m", "--monitor-targets", dest="monitored_targets", default=[".",], help="Directories or files to monitor", nargs="*")
     parser.add_argument("-t", "--testing-utility", default="nosetests")
     parser.add_argument("--shell", help="Treat the '-t' argument as a shell command", default=False, action="store_true")
     parser.add_argument("--ignore-pyc", default=False, action="store_true")
@@ -21,25 +21,21 @@ def get_parser():
     return parser
 
 def main_loop(args):
-    watcher = _build_watcher(args)
-    for change in watcher:
-        _run_nosetests(args)
+    watchers = _build_watchers(args)
+    for watcher in watchers:
+        for change in watcher:
+            _run_nosetests(args)
     return 0
 
-def _build_watcher(args):
-    accepted_extensions = args.accepted_extensions
-    if not accepted_extensions:
-        accepted_extensions = [".py"]
-        if not args.ignore_pyc:
-            accepted_extensions.append(".pyc")
-    if os.path.isdir(args.monitored_target):
+def _build_watcher(args, monitored_target, accepted_extensions):
+    if os.path.isdir(monitored_target):
         return DirectoryChangeIterator(
-            args.monitored_target,
+            monitored_target,
             sleep=args.sleep,
             accepted_extensions=accepted_extensions,
             exclude_dirs=args.exclude_dirs)
     return FileChangeIterator(
-        args.monitored_target,
+        monitored_target,
         sleep=args.sleep,
         )
 
@@ -48,7 +44,7 @@ def _run_nosetests(args):
     command.extend(args.remainder)
     cwd = args.run_in_dir
     if cwd is None:
-        cwd = args.monitored_target
+        cwd = args.monitored_targets[0]
         if not os.path.isdir(cwd):
             cwd = os.path.dirname(cwd)
     p = subprocess.Popen(command, cwd=cwd, shell=args.shell)
