474fff363a4cb201097819102af68c8000c054d0
Guy Rozendorn <guy@rzn.co.il>
2011-08-07 11:47:03 +0300

    switching from action=store,nargs=* to action=append
    Conflicts:
    
    	redgreen/scripts/__init__.py

--- redgreen/scripts/__init__.py
+++ redgreen/scripts/__init__.py
@@ -12,7 +12,7 @@ def get_parser():
     parser.add_argument("-v", "--verbose", action="store_true", default=False)
     parser.add_argument("-e", "--exclude-dir", dest="exclude_dirs", action="append", default=[])
     parser.add_argument("-d", "--run-in-dir", dest="run_in_dir", default=None, help="Specifies a directory in which to run nose (defaults to the monitored directory)")
-    parser.add_argument("-m", "--monitor-targets", dest="monitored_targets", default=[".",], help="Directories or files to monitor", nargs="*")
+    parser.add_argument("-m", "--monitor-targets", dest="monitored_targets", default=[".",], help="Directories or files to monitor", action="append")
     parser.add_argument("-t", "--testing-utility", default="nosetests")
     parser.add_argument("--shell", help="Treat the '-t' argument as a shell command", default=False, action="store_true")
     parser.add_argument("--ignore-pyc", default=False, action="store_true")
@@ -26,6 +26,15 @@ def main_loop(args):
         for change in watcher:
             _run_nosetests(args)
     return 0
+
+def _build_watchers(args):
+    accepted_extensions = [".py"]
+    if not args.ignore_pyc:
+        accepted_extensions.append(".pyc")
+    if len(args.monitored_targets) > 1 and args.monitored_targets[0] == ".":
+        # workaroud to handle the "default"
+        args.monitored_targets.pop(0)
+    return [_build_watcher(args, target, accepted_extensions) for target in args.monitored_targets]
 
 def _build_watcher(args, monitored_target, accepted_extensions):
     if os.path.isdir(monitored_target):
